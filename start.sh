5928af4c46fe0b1ae03223677cf24a3d83eb220e
Raif Coonjah <raifcoonjah@pm.me>
2022-08-03 20:10:33 +0400

    Update start.sh
    Update comments to make sure it easier to understand what each thing does. (p1)

--- start.sh
+++ start.sh
@@ -1,17 +1,17 @@
-# debugging
+# Used for Debugging, Do NOT Remove. 
 set -x
 
-# stop display manager (sddm)
+# Stopping display manager used by KDE (in this case SDDM)
 systemctl stop display-manager
 
-# unbind VTconsoles
+# Unbind VTconsoles
 echo 0 > /sys/class/vtconsole/vtcon0/bind
 echo 0 > /sys/class/vtconsole/vtcon1/bind
 
 # Unbind EFI-framebuffer
 echo efi-framebuffer.0 > /sys/bus/platform/drivers/efi-framebuffer/unbind
 
-# avoid race condition
+# Avoid race condition. Let's wait 10 seconds before unloading NKM (nvidia kernel modules)
 sleep 10
 
 # Unload NVIDIA kernel modules
@@ -20,9 +20,10 @@ modprobe -r nvidia_modeset
 modprobe -r nvidia_uvm 
 modprobe -r nvidia
 
-# unbind gpu completely
+
+# Completely unbind GPU from Host
 virsh nodedev-detach pci_0000_01_00_0
 virsh nodedev-detach pci_0000_01_00_1
 
-# load vfio
+# Load vfio
 modprobe vfio_pci
